c94e533d3bf87d94a6bf260a77c40f94fbfda8d7
HugoDS <hugodlgd@gmail.com>
2022-03-06 22:46:55 -0600

    Pipe addition

--- PIPE.c
+++ PIPE.c
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[])
 			fgets(Msg,100,FD);
 			//ret=fscanf(FD,"%d",&value);
 			//printf("This is the parent. Received value %d from child on fifo \n", value);
-			read(FD,Msg,sizeof(Msg));
+			//read(FD,Msg,sizeof(Msg));
 			printf("Message:\n %s\n",Msg);
 			//strcpy(Msg,"");
 			close(FD);

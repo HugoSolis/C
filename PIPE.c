657ffbf5edb503d80bfd63eba9abc89ea1a7dde9
HugoDS <hugodlgd@gmail.com>
2022-03-07 00:13:36 -0600

    Pipe addition

--- PIPE.c
+++ PIPE.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 {
 	printf("Starting server\n");
 	int FD;
-	char * Fifo_Server = "server_pipe";
+	char * Fifo_Server = "/pipes/server_pipe";
 	mkfifo(Fifo_Server,0666);
 	char Msg[100];
 	//strcpy(Msg,"");

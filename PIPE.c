46871bfd2a6264aeac5a3f6da532df203b271d46
HugoDS <hugodlgd@gmail.com>
2022-03-06 23:18:53 -0600

    Pipe addition

--- PIPE.c
+++ PIPE.c
@@ -19,12 +19,12 @@ int main(int argc, char const *argv[])
 	{
 		if(PID == 0){
 			printf("F\n");
-		FD = fopen(Fifo_Server,"w");
+		FD = open(Fifo_Server,"w");
 		fgets(Msg,100,stdin);
 		write(FD,Msg,strlen(Msg)+1);
 		//strcpy(Msg,"");
-		fprintf("Message:\n %s\n",Msg);
-		fclose(FD);
+		printf("Message:\n %s\n",Msg);
+		close(FD);
 		unlink(FD);
 		exit(0);}
 
@@ -32,12 +32,12 @@ int main(int argc, char const *argv[])
 			sleep(1);
 			wait(0);
 			printf("C\n");
-			FD = fopen(Fifo_Server,"r");
+			FD = open(Fifo_Server,"r");
 			fgets(Msg,100,FD);
 			//ret=fscanf(FD,"%d",&value);
 			//printf("This is the parent. Received value %d from child on fifo \n", value);
 			//read(FD,Msg,sizeof(Msg));
-			fprintf("Message:\n %s\n",Msg);
+			printf("Message:\n %s\n",Msg);
 			//strcpy(Msg,"");
 			close(FD);
 			if(strncmp(Msg, "EXIT",4) == 0)

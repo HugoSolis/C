77083118e0daadcb8d1b670e6d316377eac3590b
HugoDS <hugodlgd@gmail.com>
2022-03-06 20:37:10 -0600

    Server addition

--- Server.c
+++ Server.c
@@ -3,6 +3,17 @@
 int main(int argc, char const *argv[])
 {
 	printf("Starting server\n");
-	
+	int FD;
+	char * Fifo_Server = "/pipes/server_pipe";
+	mkfifo(Fifo_Server,0666);
+	char Msg[100];
+
+	while (true)
+	{
+		FD = open(Fifo_Server,O_WRONLY);
+		fgets(Msg,100,stdin);
+		write(FD,Msg,stlen(Msg)+1);
+		close(FD);
+	}
 	return 0;
 }

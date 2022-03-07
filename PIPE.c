31d70937b4c15b64ff7ed6b995f00fdcc85f39b9
HugoDS <hugodlgd@gmail.com>
2022-03-06 21:24:42 -0600

    Pipe addition

--- PIPE.c
+++ PIPE.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(int argc, char const *argv[])
 {
-return(0)
+	printf("Starting server\n");
+	int FD;
+	fork();
+	char * Fifo_Server = "/pipes/server_pipe";
+	mkfifo(Fifo_Server,0666);
+	char Msg[100];
+
+	while (1)
+	{
+		if(fork() == 1){
+		FD = open(Fifo_Server,O_WRONLY);
+		fgets(Msg,100,stdin);
+		write(FD,Msg,strlen(Msg)+1);
+		//strcpy(Msg,"");
+		close(FD);}
+
+		else{
+			FD = open(Fifo_Server,O_RDONLY);
+			read(FD,Msg,sizeof(Msg));
+			printf("Message:\n %s\n",Msg);
+			//strcpy(Msg,"");
+			close(FD);
+			fflush (stdout);
+			if(strncmp(Msg, "EXIT",4) == 0)
+			{
+				printf("Abort process\n");
+				break;
+			}
+		}
+	}
+	return 0;
 }
